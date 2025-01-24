b133343fcd8160c18f7612a576e5f8b8ec5835ee
--global <--global>
2025-01-24 18:42:01 +0100

    table with users

--- src/main/java/com/aplikacja/przyklad1/controller/UserController.java
+++ src/main/java/com/aplikacja/przyklad1/controller/UserController.java
@@ -4,6 +4,7 @@ package com.aplikacja.przyklad1.controller;
 import com.aplikacja.przyklad1.model.User;
 import com.aplikacja.przyklad1.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -14,12 +15,26 @@ public class UserController {
     @Autowired
     private UserRepository userRepository;
 
+//    Widok z tabelą użytkowników
     @GetMapping
-        public List<User> getAllUsers() {
-            return userRepository.findAll();
-        }
+    public String showAllUsers(Model model) {
+        model.addAttribute("allUsers", userRepository.findAll());
+        return "users";
+    }
+//    Obsługa formularza
     @PostMapping
-        public User createUser(@RequestBody User user) {
-            return userRepository.save(user);
-        }
+    public String addUser(@RequestParam String name, @RequestParam String email, Model model) {
+        User user = new User(name, email);
+        userRepository.save(user);
+        return "redirect:/users";
+    }
 }
+
+//    @GetMapping
+//        public List<User> getAllUsers() {
+//            return userRepository.findAll();
+//        }
+//    @PostMapping
+//        public User createUser(@RequestBody User user) {
+//            return userRepository.save(user);
+//        }
