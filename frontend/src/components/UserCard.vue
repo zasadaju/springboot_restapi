0f04096217b98487b1949f4a53651106eb61584e
Adrian <kontakt@adrianflak.pl>
2025-01-31 20:40:44 +0100

    consolelog

--- frontend/src/components/UserCard.vue
+++ frontend/src/components/UserCard.vue
@@ -1,27 +1,21 @@
-<template>
-  <div class="user-card">
-    <h3>{{ user.name }}</h3>
-    <p>Email: {{ user.email }}</p>
-    <router-link :to="'/users/' + user.id">🔍 Zobacz szczegóły</router-link>
-    <button @click="deleteUser">❌ Usuń</button>
-  </div>
-</template>
-
 <script>
 import apiClient from '@/api/axios.js';
 
 export default {
   props: { user: Object },
+  mounted() {
+    console.log("User w UserCard.vue:", this.user); 
+  },
   methods: {
     deleteUser() {
-      if (!this.user.id) {
-        console.error("Błąd: ID użytkownika jest undefined!");
+      if (!this.user || !this.user.id) {
+        console.error("Błąd: ID użytkownika jest undefined!", this.user);
         return;
       }
       apiClient.delete(`/users/${this.user.id}`)
         .then(() => {
           alert('Użytkownik usunięty!');
-          this.$emit('userDeleted', this.user.id); // Emisja eventu
+          this.$emit('userDeleted', this.user.id); 
         })
         .catch(error => {
           console.error('Błąd usuwania użytkownika:', error);
