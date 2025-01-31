85b3bbd40314d818d829cf3027ac32bbdb3ba793
Adrian <kontakt@adrianflak.pl>
2025-01-31 20:16:45 +0100

    websocket disable

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -7,11 +7,14 @@
 
 module.exports = {
   devServer: {
+    hot: false,
+    liveReload: false,
     proxy: {
       '/api': { //każde żądanie zaczynającie się od /api będzie przekierowane
         target: 'http://localhost:8080', //docelowy backend działa porcie 8080
         changeOrigin: true, //podmiana nagłówka origin w żądaniu
         pathRewrite: {'^/api':''}, //usuwamy /api z url przed przekazaniem do backendu
+        ws: false //wyłącza websocket
       },
     }
   }
