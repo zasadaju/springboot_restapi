e18606b786b21010cb363d681aacebe25c5ea9e2
Adrian <kontakt@adrianflak.pl>
2025-02-03 20:03:55 +0100

    origin ip

--- backend/src/main/java/com/aplikacja/przyklad1/controller/UserController.java
+++ backend/src/main/java/com/aplikacja/przyklad1/controller/UserController.java
@@ -9,6 +9,7 @@ import com.aplikacja.przyklad1.repository.UserRepository;
 import java.util.List;
 import java.util.Optional;
 
+@CrossOrigin(origins = "http://135.224.16.69:8081")
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
