a373f04a61a5f2267b26e92706f1aed5bec14a58
Adrian <kontakt@adrianflak.pl>
2025-02-03 18:48:43 +0100

    backend ip

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -11,7 +11,7 @@ module.exports = {
     liveReload: false,
     proxy: {
       '/api': { //każde żądanie zaczynającie się od /api będzie przekierowane
-        target: 'http://localhost:8080', //docelowy backend działa porcie 8080
+        target: 'http://135.224.16.69:8080', //docelowy backend działa porcie 8080
         changeOrigin: true, //podmiana nagłówka origin w żądaniu
         pathRewrite: {'^/api':''}, //usuwamy /api z url przed przekazaniem do backendu
         ws: false //wyłącza websocket
