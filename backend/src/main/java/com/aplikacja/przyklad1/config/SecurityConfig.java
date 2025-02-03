b8819d7ce4311d30dc01a81f5545aededc2919cb
Adrian <kontakt@adrianflak.pl>
2025-02-03 17:59:04 +0100

    new changes

--- backend/src/main/java/com/aplikacja/przyklad1/config/SecurityConfig.java
+++ backend/src/main/java/com/aplikacja/przyklad1/config/SecurityConfig.java
@@ -3,43 +3,45 @@ package com.aplikacja.przyklad1.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.builders.HttpSecurity;
-import org.springframework.security.config.annotation.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
 @EnableWebSecurity
-
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-    
 
-    //Konfiguracja zabezpieczeń protokołu HTTP
+    // Konfiguracja zabezpieczeń protokołu HTTP
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http 
-        .csrf().disable()
-        .authorizeRequests()
-            .antMachers("/api/users/**").authenticated()
-            .anyRequest().permitAll()
-    .and()
-    .formLogin()
-        .loginProcessUrl("/login")
-        .defaultSuccessUrl("/", true)
-        .permitAll()
-    .and()
-    .logout()
-        .logoutSuccessUrl("/login?logout")
-        .permitAll();
+        http
+            .csrf().disable()
+            .authorizeRequests()
+                .antMatchers("/api/users/**").authenticated() 
+                .anyRequest().permitAll()
+            .and()
+            .formLogin()
+                .loginProcessingUrl("/login")  
+                .defaultSuccessUrl("/", true)
+                .permitAll()
+            .and()
+            .logout()
+                .logoutSuccessUrl("/login?logout")
+                .permitAll();
     }
-    //Konfiguracja globalna dotycząca uwierzytelniania
+
+    // Konfiguracja globalna dotycząca uwierzytelniania
     @Override
-    protected void configure(AutenticationManagerBuilder auth) throws Exception {
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {  
         auth
             .inMemoryAuthentication()
             .withUser("user")
             .password(passwordEncoder().encode("password"))
             .roles("USER");
     }
+
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
