d847c3d1f4e5e5e401e23137eb07ba72921b5257
Adrian <kontakt@adrianflak.pl>
2025-02-03 20:10:44 +0100

    no login

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -4,14 +4,14 @@ const apiClient = axios.create({
   baseURL: "http://135.224.16.69:8080/api",
   httpsAgent: false,
   headers: { "Content-Type": "application/json" },
-  withCredentials: true // WYMAGANE dla Spring Security
+  withCredentials: false // WYMAGANE dla Spring Security
 });
 
-export function login(username, password) {
-  return apiClient.post("/auth/login", new URLSearchParams({ username, password }), {
-    headers: { "Content-Type": "application/x-www-form-urlencoded" }
-  });
-}
+// export function login(username, password) {
+//   return apiClient.post("/auth/login", new URLSearchParams({ username, password }), {
+//     headers: { "Content-Type": "application/x-www-form-urlencoded" }
+//   });
+// }
 
 export function getUsers() {
   return apiClient.get("/users");
