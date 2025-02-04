358655a6a57cd37fc7ecb3353dd7bb86a0337d02
Julia <zasadajulia984@gmail.com>
2025-02-04 18:22:15 +0100

    update

--- frontend/src/components/UserManagement.vue
+++ frontend/src/components/UserManagement.vue
@@ -1,53 +1,56 @@
 <template>
     <div>
-        <!-- Lista uzytkownikow -->
+        <!-- Lista użytkowników -->
         <ul v-for="user in users" :key="user.id">
             <li>{{ user.name }} - {{ user.email }}</li>
             <button @click="deleteUser(user.id)">Usuń</button>
             <button @click="editUser(user)">Edytuj</button>
         </ul>
-        <!-- Formularz dodawania nowego uzytkownika -->
+        <!-- Formularz dodawania nowego użytkownika -->
          <input v-model="newUser.name" placeholder="Imię">
          <input v-model="newUser.email" placeholder="Email">
          <button @click="addUser()">Dodaj użytkownika</button>
+
     </div>
 </template>
 
 <script>
-    import axios from 'axios';
-    export default {
-        data() {
-            return {
-                users: [],
-                newUser: {name: '', email: ''}
-            };
-            mounted()
+import axios from 'axios';
+export default {
+    data() {
+        return {
+            users: [],
+            newUser: {name: '', email: ''}
+        };
+    },
+    mounted() {
+    this.fetchUsers();
+},
+methods: {
+    fetchUsers() {
+        axios.get('http://20.121.42.213:8080/api/users')
+        .then(response => {
+            this.users = response.data;
+        });
+    },
+    addUser() {
+        axios.post('http://20.121.42.213:8080/api/users', this.newUser)
+        .then(() => {
+            this.users.push(this.newUser);
+            this.newUser = {name: '', email: ''};
             this.fetchUsers();
-        },
-        methods: {
-            fetchUsers() {
-                axios.get('http://20.121.42.213:8080/api/users')
-                .then(response => {
-                    this.users = response.data;
-                });
-            },
-            addUser() {
-                axios.post('http://20.121.42.213:8080/api/users', this.newUser)
-                .then(() =>{
-                    this.users.push(this.newUser);
-                    this.newUser = {name: '', email: ''};
-                    this.fetchUsers();
-                });
-            },
-            deleteUser(id) {
-                axios.delete('http://20.121.42.213:8080/api/users/${id}')
-                .then(() => {
-                    this.users = this.users.filter(user => user.id !== id)
-                });
-            },
-            editUser(user) {
-                this.newUser = {...user}
-            }
-        }
-    };
+        });
+    },
+    deleteUser(id) {
+        axios.detele('http://20.121.42.213:8080/api/users/${id}')
+        .then(() => {
+            this.users = this.users.filter(user => user.id !== id)
+    });
+
+    },
+    editUser(user) {
+        this.newUser = { ...user }
+    }
+    }
+};
 </script>
