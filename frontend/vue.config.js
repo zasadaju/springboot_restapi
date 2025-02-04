a839aca8e871afdfecbe76454c22209fcc1d56df
zasadaju <zasadajulia984@gmail.com>
2025-02-04 16:33:21 +0100

    Update vue.config.js

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -4,7 +4,7 @@ module.exports = {
     liveReload: false,
     proxy: {
       "/api": {
-        target: "http://135.224.16.69:8080", // Backend Spring Boot
+        target: "http://20.121.42.213:8080", // Backend Spring Boot
         secure: false, // Jeśli backend nie ma certyfikatu SSL, ustaw na false
         changeOrigin: true, // Ustawia poprawne origin dla backendu
         pathRewrite: { "^/api": "" }, // Usuwa `/api` z adresu URL
