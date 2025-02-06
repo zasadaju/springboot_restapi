53c773ab6c4d42ace82ae56cf9e81cce61dff17e
Julia <zasadajulia984@gmail.com>
2025-02-06 18:46:14 +0100

    update

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -5,31 +5,31 @@ import { BootstrapVue3, IconsPlugin } from 'bootstrap-vue-3'
 import 'bootstrap/dist/css/bootstrap.css'
 import { createI18n } from 'vue-i18n';
 
-function loadLocalMessages() {
+function locadLocaleMessages() {
     const locales = require.context('./assets/i18n', true, /[A-Za-z0-9-_,\s]+\.json$/i);
     const messages = {};
     locales.keys().forEach(key => {
-        const matched = key.match(/[A-Za-z0-9-_]+)\./i);
-        if(matched && matched.lenght > 1) {
+        const matched = key.match(/([A-Za-z0-9-_]+)\./i);
+        if (matched && matched.length > 1) {
             const locale = matched[1];
             messages[locale] = locales(key).default;
         }
     });
     return messages;
 }
- 
-const messages = {
-        en: {
-            message: {
-                hello: 'Hello World'
-            }
-    },
-        pl: {
-            message: {
-                hello: 'Cześć'
-            }
-    }
-};
+
+// const messages = {
+//         en: {
+//             message: {
+//                 hello: 'Hello World'
+//             }
+//     },
+//         pl: {
+//             message: {
+//                 hello: 'Cześć'
+//             }
+//     }
+// };
  
 const i18n = createI18n({
         locale: 'en',
